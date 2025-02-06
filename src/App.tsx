2b14ba2295eb928fa8397f84b8010cd41df1bb3c
joel-alvarado <noel.vargas5@upr.edu>
2025-02-05 22:39:19 -0400

    add logos, pushed footer and title to middle and bottom.

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Questionnaire from "./components/Questionaire.tsx";
+import Header from "./components/Header";
+import Title from "./components/Title";
+import Footer from "./components/Footer";
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-     <h1> Escala de seguridad alimentaria</h1>
-     <Questionnaire/>
-    </>
+    <div className='app-container'>
+    <Header/>
+    <Title/>
+    <Footer/>
+     {/* <h1> Escala de seguridad alimentaria</h1>
+     <Questionnaire/> */}
+    </div>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,30 @@ import React from "react";
 import logo1 from "../assets/CienciasMedicasUPR.png";
 import logo2 from "../assets/UCienciasPublicas.png";
 
+
 const Header: React.FC = () => {
   return (
     <header className="header">
-      <div className="logos">
-        <img src={logo1} alt="University Logo 1" className="logo" />
-        <img src={logo2} alt="University Logo 2" className="logo" />
+      <div className="header-container">
+        {/* Left Logo */}
+        <div className="logo-container">
+          <img src={logo1} alt="University Logo 1" className="logo" />
+        </div>
+
+        {/* Centered Title & Subtitle */}
+        <div className="header-text">
+          <h1 className="subtitle">Universidad de Puerto Rico</h1>
+          <h2 className="subtitle">
+            Recinto de Ciencias Médicas<br />
+            Escuela Graduada de Salud Pública
+          </h2>
+        </div>
+
+        {/* Right Logo */}
+        <div className="logo-container">
+          <img src={logo2} alt="University Logo 2" className="logo" />
+        </div>
       </div>
-      <h1 className="title">Universidad de Puerto Rico</h1>
-      <h2 className="subtitle">Recinto de Ciencias Médicas<br/>Escuela Graduada de Salud Pública</h2>
     </header>
   );
 };
