953b5f3d67c961937b9b319ed1ecca1256086bd6
noel-vargas <noel.vargas5@upr.edu>
2025-04-10 20:05:02 -0400

    Create surversubmission model and create each question as a forms.ChoiceField

--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField  # or models.JSONField for Django 3.1+
 
-# Create your models here.
+class SurveySubmission(models.Model):
+    submitted_at = models.DateTimeField(auto_now_add=True)
+    data = models.JSONField()  # to store all answers
+
+    def __str__(self):
+        return f"SurveySubmission #{self.id} at {self.submitted_at}"
