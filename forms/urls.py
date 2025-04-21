358630da6606034c7a780105773f9cb386da9d4f
noel-vargas <noel.vargas5@upr.edu>
2025-04-20 21:20:57 -0400

    created form_builder and moved the questions as json format

--- forms/urls.py
+++ forms/urls.py
@@ -1,18 +1,9 @@
 from django.urls import path
-from .views import (
-    gracias_view
-)
-from .views import (
-    escala_part1_view, escala_part2_view, escala_part3_view, escala_part4_view
-)
-from .views import EscalaWizard, escala_forms
-
-from .views import ConsentimientoWizard, consentimiento_forms
-
+from .views import ConsentimientoWizard, EscalaWizard, gracias_view
+from .views import consentimiento_forms, escala_forms
 
 urlpatterns = [
-    path('consentimiento/', ConsentimientoWizard.as_view(consentimiento_forms), name='consentimiento_wizard'),
-    path('gracias/', gracias_view, name='gracias'),
-    path('escala/', EscalaWizard.as_view(escala_forms), name='escala_wizard'),
-
+    path("consentimiento/", ConsentimientoWizard.as_view(consentimiento_forms), name="consentimiento_wizard"),
+    path("escala/", EscalaWizard.as_view(escala_forms), name="escala_wizard"),
+    path("gracias/", gracias_view, name="gracias"),
 ]
